c3f6454c9881efddc7e41352105d569e395a03df
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2022-08-19 18:10:07 -0500

    init commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-// Call helloWorld()
-function helloWorld() {
-    alert("Hello World");
+// Get user data
+// validate data
+// grab user data from input fields 
+
+function getData() {
+    let loanAmount = document.getElementById("loanAmount").value
+    let payments = document.getElementById("loanPayments").value
+    let loanRate = document.getElementById("loanRate").value
+    displayData(loanAmount, payments, loanRate)
+}
+
+// Calc user data
+// write formula for needed data
+// loop over data
+// store data in an object or array
+
+
+// Display data
+// loop over object/array
+// create tr with td's
+// input created node into table
+
+function displayData(loanAmount) {
+    document.getElementById("totalPrincipal").innerHTML = `${loanAmount}`
 }
